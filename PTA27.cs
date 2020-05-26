4d10e8b7d7d70f3ca171fd1000c5d07b3c25af93
bboygun <491476487@qq.com>
2020-05-26 21:08:17 +0800

    PTA question 27, 统计工龄

---
+++ PTA27.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PTA27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string str = Console.ReadLine();
+            string[] strs = str.Split(new string[] { " " }, StringSplitOptions.None);
+            int[] ages = new int[51];
+            foreach(string age in strs)
+            {
+                int tmp = Convert.ToInt32(age);
+                ages[tmp]++;
+            }
+            for(int i = 0;i<=ages.Length-1;i++)
+            {
+                if (ages[i] != 0) Console.WriteLine("{0}:{1}", i, ages[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
